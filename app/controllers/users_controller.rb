93933c8a7659e5034600daae3bf4cd5879b402e6
lyuhas0 <lyuhas0@chicagobooth.edu>
2020-08-10 12:51:00 +0000

    Finished users READ only

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
 
   def index
+    @users = User.all.order({ :username => :asc })
     render({ :template => "user_templates/index.html.erb" })
   end
 
   def details
+    user_id = params.fetch("id")
+    @user = User.where({ :id => user_id }).at(0)
+
+    @user_photos = Photo.where({ :owner_id => user_id })
+
     render({ :template => "user_templates/details.html.erb" })
   end
 
